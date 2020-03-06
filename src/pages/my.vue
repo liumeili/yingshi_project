bfeb9bb81b4783f26de8e8dc1d3dfc86049671d9
shenfeidong <408815032@qq.com>
2020-03-06 14:05:12 +0800

    sfd

--- src/pages/my.vue
+++ src/pages/my.vue
@@ -221,7 +221,7 @@ export default {
         top: 0;
         left: 0;
         width: 100%;
-        height: 330px;
+        height: 360px;
         background: linear-gradient(to top,rgba(#0D1225, 1),rgba(#0D1225, 0));
         .editMyinfo{
           float: right;
